1f901a13247218ea81d3d11db7d1aa30a5da99a8
kkjos <k19951227@gmail.com>
2021-12-07 03:17:28 +0000

    配送先一覧・新規投稿

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,20 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.where(customer_id: current_customer.id)
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to request.referer
   end
 
   def edit
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end
