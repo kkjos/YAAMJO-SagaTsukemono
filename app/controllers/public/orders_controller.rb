07975a5bcd75aa6c21230388762b64ae83c619e9
kkjos <k19951227@gmail.com>
2021-12-07 12:38:11 +0000

    注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,7 +1,34 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
-  
+
+  def confirm
+    @order = Order.new(order_params)
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @order.payment_method = params[:order][:payment_method]
+    @order.shipping_cost = 800
+
+    if params[:order][:address_option] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
+
+    session[:order] = @order
+    redirect_to confirm_view_orders_path
+  end
+
+  def confirm_view
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @order = Order.new(session[:order])
+  end
+
   def create
   end
 
@@ -13,10 +40,9 @@ class Public::OrdersController < ApplicationController
 
   def complete
   end
-  
-  def confirm
-  end
 
-  def comfirm_view
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method)
   end
 end
