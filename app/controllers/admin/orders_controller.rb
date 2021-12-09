193ffe47f5843ae789baaae0875194643ce2c23c
kkjos <k19951227@gmail.com>
2021-12-09 08:00:35 +0000

    注文履歴一覧・詳細ページ・ステータス変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,17 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to request.referer
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
