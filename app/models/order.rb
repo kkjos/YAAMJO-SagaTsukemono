97ab64696bc5c4509dd32fa778a18839de7554ef
kkjos <k19951227@gmail.com>
2021-12-09 08:43:02 +0000

    enumの導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_for_deposit:0, payment_confirmation:1, production:2, shipped:3}
 end
