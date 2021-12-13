716b90699e7e1e9adb7977695cd7612e519c67b6
kkjos <k19951227@gmail.com>
2021-12-13 03:19:27 +0000

    public-cart_itemレイアウト・重複作動

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,6 +6,7 @@ class Public::CartItemsController < ApplicationController
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
+    flash[:notice] = '商品の数量を変更しました。'
     redirect_to request.referer
   end
 
@@ -23,9 +24,16 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
-    flash[:notice] = 'カートに商品を追加しました。'
-    redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path, notice: "商品の数量が変更されました"
+    else
+      @cart_item.save
+      flash[:notice] = 'カートに商品を追加しました。'
+      redirect_to cart_items_path
+    end
   end
 
   private
