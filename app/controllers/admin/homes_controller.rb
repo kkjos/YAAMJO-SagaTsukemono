ca104bacca70a381d162821c91ff384e176477b8
kkjos <k19951227@gmail.com>
2021-12-19 04:11:38 +0000

    [add]admin-home

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,10 @@
 class Admin::HomesController < ApplicationController
   def top
+    @today_orders = Order.where(created_at: Time.zone.today.all_day)
+    @today_order_details = OrderDetail.where(created_at: Time.zone.today.all_day)
+    @month_orders = Order.where(created_at: Time.zone.today.all_month)
+    @month_order_details = OrderDetail.where(created_at: Time.zone.today.all_month)
+    @undispatched_orders = Order.where.not(status: 2)
+    @waiting_for_deposit_order = Order.where(status: 0)
   end
 end
