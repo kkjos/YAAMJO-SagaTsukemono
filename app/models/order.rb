f1b62f0bd948c0adc982e252dddf56c4c481ef2d
kkjos <k19951227@gmail.com>
2021-12-20 04:12:47 +0000

    [add]validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,11 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
 
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
   validates :address, presence: true
   validates :name, presence: true
+  validates :shipping_cost, presence: true
+  validates :grand_total, presence: true
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_deposit:0, production_start:1, ready_to_ship:2, shipped:3}
